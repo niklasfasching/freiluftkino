b03f89eca36fe59eab5e2d06b1403113dee797dc
Niklas Fasching <niklas.fasching@gmail.com>
2021-06-22 20:34:38 +0200

    Incrementally build history to prevent history length affecting runtime
    ```
    $ time python3 post_process.py
    real	0m1,711s
    
    $ time python3 post_process.py
    real	0m0,410s
    ```

--- post_process.py
+++ post_process.py
@@ -2,26 +2,29 @@ import git # pip install GitPython
 import json
 
 
-def file_versions(path):
-    for commit in reversed(list(git.Repo().iter_commits(paths=path))):
+def file_versions(path, n):
+    for commit in reversed(list(git.Repo().iter_commits(paths=path, max_count=n))):
         yield (commit.committed_date, commit.tree[path].data_stream.read())
 
 
-def build_history():
-    history = {}
-    for timestamp, contents in file_versions("docs/showsByCinema.json"):
+def write_history(path):
+    with open(path) as f:
+        history_json = f.read()
+        history = json.loads(history_json) if history_json != "" else {}
+    for timestamp, contents in file_versions("docs/showsByCinema.json", 10 if history_json != "" else None):
         timestamp_ms = timestamp * 1000
         for cinema, shows in json.loads(contents).items():
             for show in shows:
                 if history.get(show["url"]):
-                    _, available, reserved = history[show["url"]][-1]
-                    if available == show.get("available") and reserved == show.get("reserved"):
+                    previous = history[show["url"]][-1]
+                    if previous[0] >= timestamp_ms or (previous[1] == show.get("available") and previous[2] == show.get("reserved")):
                         continue
                 else:
                      history[show["url"]] = []
                 history[show["url"]].append((timestamp_ms, show.get("available"), show.get("reserved")))
-    return history
+    with open(path, "w") as f:
+        json.dump(history, f, indent=2)
 
 
-with open("docs/showHistory.json", "w") as f:
-    json.dump(build_history(), f, indent=2)
+if __name__ == "__main__":
+    write_history("docs/showHistory.json")
