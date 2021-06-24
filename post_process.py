735a0a4df4292f545235f308845a00760bdb7b11
Niklas Fasching <niklas.fasching@gmail.com>
2021-06-24 21:31:47 +0200

    Trim unnecessary entries from show history and rename to history.json
    can't use id for table key bc format of that changed multiple times

--- post_process.py
+++ post_process.py
@@ -1,6 +1,6 @@
 import git # pip install GitPython
 import json
-
+import pathlib
 
 def file_versions(path, n):
     for commit in reversed(list(git.Repo().iter_commits(paths=path, max_count=n))):
@@ -8,6 +8,7 @@ def file_versions(path, n):
 
 
 def write_history(path):
+    pathlib.Path(path).touch(exist_ok=True)
     with open(path) as f:
         history_json = f.read()
         history = json.loads(history_json) if history_json != "" else {}
@@ -15,16 +16,21 @@ def write_history(path):
         timestamp_ms = timestamp * 1000
         for cinema, shows in json.loads(contents).items():
             for show in shows:
+                reserved, available = show.get("reserved", 0), show.get("available", 0)
                 if history.get(show["url"]):
                     previous = history[show["url"]][-1]
-                    if previous[0] >= timestamp_ms or (previous[1] == show.get("available") and previous[2] == show.get("reserved")):
+                    open_seating = previous[1] == -1 or available == -1
+                    if (previous[0] >= timestamp_ms or # already in history
+                        (not open_seating and previous[2] == reserved and (previous[1] == available or available > 0)) or # availability didn't change
+                        (not open_seating and reserved == 0 and available == 0) or # bad data
+                        (open_seating and available == previous[1])): # availability didn't change
                         continue
                 else:
                      history[show["url"]] = []
-                history[show["url"]].append((timestamp_ms, show.get("available"), show.get("reserved")))
+                history[show["url"]].append((timestamp_ms, available, reserved))
     with open(path, "w") as f:
         json.dump(history, f, indent=2)
 
 
 if __name__ == "__main__":
-    write_history("docs/showHistory.json")
+    write_history("docs/history.json")
